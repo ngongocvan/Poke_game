85097b98632d1b7080e1c07f47cfafad5be811f9
ngongocvan <ngocvan016355@gmail.com>
2024-05-27 16:06:47 +0700

    day

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-
 export default function Page2() {
   return (
-    <div className='hello-page'>
-        <img src="./img1.png" alt=""/>
-        <h2 className='title'>What is FUDU2?</h2>
-         <div className='description'>
-            <p>$FUDU - native token on Ton Blockchain. Tired of seeing DUCKs on your timeline?Sick of seeing DUCKs using holders as exit liquidty?</p>
-         </div>
+    <div className="hello-page">
+      <img src="./img1.png" alt="" />
+      <h2 className="title">What is FUDU2?</h2>
+      <div className="description">
+        <p>
+          $FUDU - native token on Ton Blockchain. Tired of seeing DUCKs on your
+          timeline?Sick of seeing DUCKs using holders as exit liquidty?
+        </p>
+      </div>
     </div>
-  )
+  );
 }
