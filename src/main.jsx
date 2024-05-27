bf4719d4f899b9f632a835d2e1359d3cb9410392
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2024-05-27 15:25:19 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'; // Global styles if needed
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
